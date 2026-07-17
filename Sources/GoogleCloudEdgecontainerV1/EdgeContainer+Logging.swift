cd0bfbc043998ac373a66961a137b6179ff7b787
Carlos O'Ryan <coryan@google.com>
2026-07-17 13:12:18 -0400

    chore: update librarian circa 2026-07-17 (#336)
    In this version librarian avoids conflicts for the `Logging` module from
    `swift-log` and any type called `Logging` in the imported dependencies.
    This removes a number of unwanted / unnecessary imports.
    
    Discovery-based LROs also had too many overloads. Only the overload
    returning the pollable helper is needed, that is what we do for AIP-151
    LROs.

--- Sources/GoogleCloudEdgecontainerV1/EdgeContainer+Logging.swift
+++ Sources/GoogleCloudEdgecontainerV1/EdgeContainer+Logging.swift
@@ -18,21 +18,19 @@ import Foundation
 #if canImport(FoundationNetworking)
   import FoundationNetworking
 #endif
-
-import GoogleCloudAuth
-import GoogleCloudGax
 import GoogleCloudLocation
 import GoogleCloudWkt
 import GoogleLongrunning
 import GoogleRpc
-import Logging
+import GoogleCloudGax
+import struct Logging.Logger
 
 extension Clients {
   final class EdgeContainerLogging: EdgeContainerStub {
     let inner: any EdgeContainerStub
-    let logger: Logging.Logger
+    let logger: Logger
 
-    public init(_ inner: any EdgeContainerStub, logger: Logging.Logger) {
+    public init(_ inner: any EdgeContainerStub, logger: Logger) {
       var logger = logger
       logger[metadataKey: "gcp.artifact.id"] = "GoogleCloudEdgecontainerV1"
       logger[metadataKey: "gcp.client.service"] = "edgecontainer"
--- Sources/GoogleCloudEdgecontainerV1/EdgeContainer.swift
+++ Sources/GoogleCloudEdgecontainerV1/EdgeContainer.swift
@@ -18,14 +18,11 @@ import Foundation
 #if canImport(FoundationNetworking)
   import FoundationNetworking
 #endif
-
-import GoogleCloudAuth
-import GoogleCloudGax
 import GoogleCloudLocation
 import GoogleCloudWkt
 import GoogleLongrunning
 import GoogleRpc
-import Logging
+import GoogleCloudGax
 
 /// EdgeContainer API provides management of Kubernetes Clusters on Google Edge
 /// Cloud deployments.
